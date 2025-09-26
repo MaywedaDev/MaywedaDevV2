366dd0daeff427d97439b0fed5cf10e51e23c5f1
Enyo Onuche <enyoonuche1@gmail.com>
2025-09-26 21:53:30 +0100

    Added scrolltrigger funtions and used matchmedia feature to create different scroll experience for mobile and desktop in experience section

--- src/components/GallerySection.vue
+++ src/components/GallerySection.vue
@@ -1,15 +1,18 @@
 <template>
   <div
     ref="gallery"
-    class="px-[112px] py-12 border-t border-[#484848] w-full lg:flex justify-between"
+    class="lg:px-[112px] py-12 border-t border-[#484848] w-full lg:flex justify-between overflow-hidden"
   >
-    <div ref="titleText" class="w-full max-w-[640px]">
+    <div ref="titleText" class="w-full ml-6 md:ml-16 lg:mx-0 lg:max-w-[640px]">
       <h2 class="text-red text-[64px] tBebas">Experience</h2>
       <div
-        class="w-full max-w-[640px] rounded-lg bg-grey-100 slider-cont"
+        class="w-full max-w-[450px] mx-auto lg:max-w-[640px] rounded-lg bg-grey-100 slider-cont"
       ></div>
     </div>
-    <div class="xl:max-w-[600px] max-w-[380px] space-y-14 pl-12">
+    <div
+      ref="expCont"
+      class="my-16 px-6 md:px-16 lg:px-0 lg:my-0 lg:max-w-[600px] flex flex-row lg:flex-col pl-12 exp-cont gap-x-5 gap-y-14"
+    >
       <ExpItem
         company-name="Company Name"
         job-title="Fullstack Developer"
@@ -34,17 +37,48 @@ gsap.registerPlugin(ScrollTrigger);
 
 const titleText = ref<HTMLDivElement>();
 const gallery = ref<HTMLDivElement>();
+const expCont = ref<HTMLDivElement>();
+
+const mm = gsap.matchMedia();
 
 onMounted(() => {
-  if (!titleText.value || !gallery.value) return;
-  ScrollTrigger.create({
-    trigger: gallery.value,
-    start: "top top",
-    end: "bottom 80%",
-    // markers: true,
-    pin: titleText.value,
-    scrub: true,
+  mm.add("(max-width:1023px)", (context) => {
+    if (expCont.value)
+      gsap.to(expCont?.value, {
+        xPercent: -100,
+        x: () => window.innerWidth,
+        ease: "none", // <-- IMPORTANT!
+        scrollTrigger: {
+          pin: gallery.value,
+          trigger: gallery.value,
+          start: "left left",
+          end: () => `+=${expCont.value?.offsetWidth} bottom`,
+          scrub: 1,
+          markers: true,
+        },
+      });
   });
+
+  mm.add("(min-width:1024px)", () => {
+    if (expCont.value)
+      ScrollTrigger.create({
+        trigger: gallery.value,
+        start: "top top",
+        end: "bottom 80%",
+        // markers: true,
+        pin: titleText.value,
+        scrub: true,
+      });
+  });
+  // if (!titleText.value || !gallery.value) return;
+  // ScrollTrigger.create({
+  //   trigger: gallery.value,
+  //   start: "top top",
+  //   end: "bottom 80%",
+  //   // markers: true,
+  //   pin: titleText.value,
+  //   scrub: true,
+  // });
 });
 
 const desc = `Lorem ipsum dolor sit amet consectetur. Eu laoreet sit vitae auctor a sociis sed sodales donec. Sit lacus turpis id ornare`;
@@ -53,5 +87,11 @@ const desc = `Lorem ipsum dolor sit amet consectetur. Eu laoreet sit vitae aucto
 <style>
 .slider-cont {
   aspect-ratio: 16 / 12;
+}
+
+@media screen and (max-width: 1023px) {
+  .exp-cont {
+    width: 2908px;
+  }
 }
 </style>

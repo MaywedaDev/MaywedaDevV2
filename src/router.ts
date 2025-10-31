82ec383497039cf9879c1b3ba8ae2b9b1eec1221
Enyo Onuche <enyoonuche1@gmail.com>
2025-10-31 22:51:58 +0100

    Removed Github view from router

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,17 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from './views/HomeView.vue'
-import GithubView from './views/GithubView.vue'
-import NotFound from './views/NotFound.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "./views/HomeView.vue";
+import GithubView from "./views/GithubView.vue";
+import NotFound from "./views/NotFound.vue";
 
 const routes = [
-    {path: '/', name: 'home',  component: HomeView},
-    {path: '/github', name: 'about', component: GithubView},
-    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
-]
+  { path: "/", name: "home", component: HomeView },
+  // {path: '/github', name: 'about', component: GithubView},
+  { path: "/:pathMatch(.*)*", name: "not-found", component: NotFound },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
